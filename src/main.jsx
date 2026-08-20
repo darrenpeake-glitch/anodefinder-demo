d3e738511f323b2f52eeb18ae7e1573b4babf48e
Darren Peake <darrenpeake@gmail.com>
2026-08-20 16:07:35 +0100

    Load catalogue status styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './sprint01.css'
+import './catalogue-status.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
