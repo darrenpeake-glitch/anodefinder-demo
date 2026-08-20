b89e1e3aa774359ebc4eadd78735fffb220d0118
Darren Peake <darrenpeake@gmail.com>
2026-08-20 19:52:54 +0100

    Limit product image test to Alpha One Gen II

--- src/data/productImages.js
+++ src/data/productImages.js
@@ -1,16 +1,7 @@
-import mercuryAlphaOneGen2 from '../product-images/mercury-alpha-one-gen2.svg'
-import sleipner01050 from '../product-images/sleipner-01050.svg'
-import volvoDphDprKit from '../product-images/volvo-dph-dpr-kit.svg'
+import mercuryAlphaOneGen2 from '../product-images/mercury-alpha-one-gen2-test.svg'
 
 const imageBySku = new Map([
   ['KITALPHAONE', mercuryAlphaOneGen2],
-  ['KITALPHAONE/AL', mercuryAlphaOneGen2],
-  ['KITALPHAONE-MG', mercuryAlphaOneGen2],
-  ['01050', sleipner01050],
-  ['01050AL', sleipner01050],
-  ['KITVOLVODPH/DPR', volvoDphDprKit],
-  ['KITVOLVODPH-DPR-AL', volvoDphDprKit],
-  ['KITVOLVODPH-DPR-MG', volvoDphDprKit],
 ])
 
 export function productImage(product) {
