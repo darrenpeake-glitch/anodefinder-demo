9ca7d8f2a0ba84894ce6c0ff27c5c7ce486a5559
Darren Peake <darrenpeake@gmail.com>
2026-08-20 18:36:13 +0100

    Load order simulation styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './sprint01.css'
 import './catalogue-status.css'
 import './myboat.css'
+import './order-simulation.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
