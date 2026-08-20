37a6e9497363fd26a7ca034531a3e572ac77aa20
Darren Peake <darrenpeake@gmail.com>
2026-08-20 16:38:13 +0100

    Load catalogue technical overlays before rendering app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import './data/catalogueBootstrap.js'
 import App from './App.jsx'
 import './styles.css'
 import './sprint01.css'
